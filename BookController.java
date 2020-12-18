c7db5475632e58b9130b61aeeca863041c356341
如风不如常 <380431932@qq.com>
2020-12-18 10:32:35 +0800

    ok

--- BookController.java
+++ BookController.java
@@ -1,13 +1,8 @@
-package com.zking.ssm.controller;
 
-@controller
 public class BookController{
-<<<<<<< HEAD
-	bbbb  
-=======
+
 	public String writting(){
 		return "aaabbb";
-	}  
->>>>>>> 1aeda6c202b02d9199c993ea3bc3433c71224e54
-  
+	}
+
 }
